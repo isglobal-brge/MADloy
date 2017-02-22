a42886ee5581a7a103bf85b40e1022e81e1da2ef
Marcos <dwalinybalin@gmail.com>
2017-02-22 19:18:10 +0100

    getMedian and plot.MADloy modified to work with the autosomal reference
    modified the functions getMedian.R and plot.MADloy to work with the
    autosomal reference

--- R/plot.MADloy.R
+++ R/plot.MADloy.R
@@ -1,16 +1,13 @@
 #' @method plot MADloy
 #' @export
 plot.MADloy <- function(x, ...) {
-par(mfrow=c(1,2))
   data <- MADloy:::getMedian(x)
   xx <- data[, 1]
   yy.1 <- data[, 2]
-  yy.2 <- data[, 3]
   d.1 <- xx - yy.1
-  d.2 <- xx - yy.2
   ss <- 1:nrow(data)
-  plot.default(ss, d.1, type = "n", xlab = "Individuals", ylab = paste0("Median LRR difference (",GenomeInfoDb::seqnames(x$par$target.region), " - ", GenomeInfoDb::seqnames(x$par$ref.region.1), ")"), ...)
+  ref <- GenomeInfoDb::seqnames(x$par$ref.region)
+  ref <- ifelse( length(GenomeInfoDb::seqnames(x$par$ref.region)) == 22 , "Autosomes", paste(GenomeInfoDb::seqnames(x$par$ref.region), collapse="_"))
+  plot.default(ss, d.1, type = "n", xlab = "Individuals", ylab = paste0("Median LRR difference (",GenomeInfoDb::seqnames(x$par$target.region), " - ", ref, ")"), ...)
   points(ss, d.1, pch = 16)
-  plot.default(ss, d.2, type = "n", xlab = "Individuals", ylab = paste0("Median LRR difference (",GenomeInfoDb::seqnames(x$par$target.region), " - ", GenomeInfoDb::seqnames(x$par$ref.region.2), ")"), ...)
-  points(ss, d.2, pch = 16)
 } 
